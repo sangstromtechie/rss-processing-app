5fd2b0b9eb0fdd283e8621d14d797510f48db0bd
Unknown <cwenham@gmail.com>
2018-02-05 10:16:26 -0600

    Added RSSActivity

---
+++ app/src/main/java/com/cwenham/rssprocessing/RSSActivity.java
@@ -0,0 +1,38 @@
+package com.cwenham.rssprocessing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RSSActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rss);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
